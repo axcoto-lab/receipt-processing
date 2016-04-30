2b548fe5629a64dcbbc7eed6f580fa616842d2dc
Vinh <kurei@axcoto.com>
2016-04-30 13:22:38 -0700

    Show total diff for a receipt

--- src/receipts/reader.clj
+++ src/receipts/reader.clj
@@ -51,33 +51,34 @@
 
 (defn diff-line
   "Process a line of receipt"
-  [line]
+  [line prices]
   (def parts (clojure.string/split line #"\s+"))
   (let [[code paid]
       (if (= (count (last parts)) 1)
         ; Has random char at the end
         [(nth parts (- (count parts) 3)) (nth parts (- (count parts) 2))]
         [(nth parts (- (count parts) 2)) (last parts)])]
-    (Float/parseFloat paid)))
+      (- (get prices code) (Float/parseFloat paid))))
 
 (defn parse-store-name
   [line]
   (clojure.string/trim line))
+
 (defn diff-receipt
   "Process receipt"
-  [receipt price-list]
+  [receipt prices]
   (def rc (atom {}))
   (def lines (clojure.string/split-lines receipt))
-  (def store-name (parse-store-name (first lines)))
-  (def prices p/fetch)
-  (loop [index 1 total 0]
+  (def store (parse-store-name (first lines)))
+  (def plusminus (loop [index 1 total 0]
     (if (< index (- (count lines) 1))
       (let [[j paid]
               (if (.contains (nth lines (+ index 1)) "VOIDED PRODUCT")
                 [2 0]
-                [1, (diff-line (nth lines index))])]
-              (recur (+ index j) (+ total paid)))
+                [1, (diff-line (nth lines index) prices)])]
+        (recur (+ index j) (+ total paid)))
       total)))
+  {store plusminus})
 
 (defn scan
   "Read from data"
@@ -85,9 +86,9 @@
   (def d (clojure.java.io/file directory))
   (let [fs (file-seq d)
         prices (p/fetch "./data/prices.csv")]
-    (doseq [f fs]
-      (if (and (.isFile f) (not (= "prices.csv" (.getName f))))
-        (let [csv-path (.getParent f) "/" (.getName f)]
-          (println csv-path)
-          (diff-receipt (slurp (str (.getParent f) "/" (.getName f))) prices) "\n" ))
-    )))
+    (println (merge-with +
+      (map (fn [f]
+          (if (and (.isFile f) (not (= "prices.csv" (.getName f))))
+            (let [csv-path (str (.getParent f) "/" (.getName f))]
+              (diff-receipt (slurp csv-path) prices))))
+         fs)))))
